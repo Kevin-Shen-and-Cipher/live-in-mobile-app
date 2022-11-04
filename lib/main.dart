c286f0f2249c3749b7b5e7d3ce971fb21c7b9f84
Dragon <C109151163@nkust.edu.tw>
2022-11-04 22:17:15 +0800

    change launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dropdown_button_plus/dropdown_plus.dart' as plus;
 
-
-
 import 'package:live_in/page/infoPage.dart';
 import 'page/HomePage.dart';
 import 'page/SearchPage.dart';
@@ -12,8 +10,47 @@ import 'page/SearchPage.dart';
 
 
 void main()=>runApp(MaterialApp(title: 'app',
-  home: new MainPage(),
+  home: new MyApp(),
 ));
+
+// MyApp
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      //future: Future.delayed(Duration(seconds: 3)),
+      builder: (context, AsyncSnapshot snapshot) {
+        // Loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            home: SplashPage(),
+          );
+        }
+        // Main
+        else {
+          return MaterialApp(
+            home: MainPage(),
+          );
+        }
+      },
+    );
+  }
+}
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF333333),
+      body: Center(
+        child: Icon(
+          Icons.pets,
+          size: MediaQuery.of(context).size.width * 0.7,
+        ),
+      ),
+    );
+  }
+}
+
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
