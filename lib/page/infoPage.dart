506065fedb0d8fb1897bdd53ad7f0ab2f19a3d0d
Dragon <C109151163@nkust.edu.tw>
2022-11-14 11:52:14 +0800

    info page update

--- lib/page/infoPage.dart
+++ lib/page/infoPage.dart
@@ -13,10 +13,77 @@ class infoPage extends StatelessWidget {
         ),
         body: ListView(
           children: [
+            SizedBox(height: 20,),
+            Container(
+              height: 100,
+              padding: EdgeInsets.only(top: 60,left: 80,right: 80,bottom: 0),
+              child:ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: () =>showAboutUs(context),
+                child: Text('About us'),
+              ) ,
+            ),
+            Container(
+              height: 100,
+              padding: EdgeInsets.only(top: 60,left: 80,right: 80,bottom: 0),
+              child:ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: () { },
+                child: Text('Help'),
+              ) ,
+            ),
+            Container(
+              height: 100,
+              padding: EdgeInsets.only(top: 60,left: 80,right: 80,bottom: 0),
+              child:ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: () { },
+                child: Text('Game'),
+              ) ,
+            ),
+            Container(
+              height: 100,
+              padding: EdgeInsets.only(top: 60,left: 80,right: 80,bottom: 0),
+              child:ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: () { },
+                child: Text('Git Hub'),
+              ) ,
+            ),
 
           ],
         )
     );
 
   }
+}
+showAboutUs(BuildContext context) {
+  // Init
+  AlertDialog dialog = AlertDialog(
+    title: Text("組員：\n鄭政文、沈育安、劉官瑜、\n丁襄Dragon、許哲晟"),
+    actions: [
+      ElevatedButton(
+        child: Text("Close"),
+        onPressed: () =>Navigator.of(context,rootNavigator: true).pop(),
+      ),
+    ],
+  );
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return dialog;
+      }
+  );
 }
